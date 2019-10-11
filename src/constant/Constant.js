568c565e1f6d4ed920c045b19a83b974c128b0af
沈炜 <277469513@qq.com>
2019-10-11 14:55:14 +0800

    修改token

--- src/constant/Constant.js
+++ src/constant/Constant.js
@@ -5,7 +5,7 @@ const Constant = {
         // baseService: {name: '基础服务', path: '/api/base/'},
         // userService: {name: '用户服务', path: '/api/user/'},
     },
-    DEFAULT_API_TOKEN: '1121ce780e1ce4319df82fb123125801a90b01f',
-    DEFAULT_API_SECRET:'1ce780e1ce4319df82fb5801a90b01f47a3d59c741c0b63e19de1d905233e1b1'
+    DEFAULT_API_TOKEN: '19c735ba3d4b4778e2213rwer123123085f120e0fda5er2af67f19f12121',
+    DEFAULT_API_SECRET:'2ce780e1ce4319df82fb5801a90b01f47a3d59c741c0b63e19de1d905233e1b1'
 };
 module.exports = Constant;

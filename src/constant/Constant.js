1a5c516654a346e6a36ba94356fc38db2651d109
沈炜 <277469513@qq.com>
2020-11-02 15:14:32 +0800

    fix

--- src/constant/Constant.js
+++ src/constant/Constant.js
@@ -5,7 +5,7 @@ const Constant = {
         // //zuul: {name: '网关', path: '/api/'},
         // system: {name: '系统管理服务', path: '/api/system/'},
     },
-    DEFAULT_API_TOKEN: '19c735ba3d4b4778e2213rwer123123085f120e0fda5er2af67f19f12121',
-    DEFAULT_API_SECRET:'2ce780e1ce4319df82fb5801a90b01f47a3d59c741c0b63e19de1d905233e1b1'
+    DEFAULT_API_TOKEN: 'c735ba3d4b4778e2213rwer123123085f120e0fda5er2af67f19f1212169',
+    DEFAULT_API_SECRET:'e780e1ce4319df82fb5801a90b01f47a3d59c741c0b63e19de1d905233e1b12c'
 };
 module.exports = Constant;
